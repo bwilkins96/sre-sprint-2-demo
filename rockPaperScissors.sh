dd4f1761aa0270d8b7d9fceba76ad5252d5556a4
Benjamin Wilkins <104269697+bwilkins96@users.noreply.github.com>
2025-03-14 16:27:55 -0400

    functions outline

--- rockPaperScissors.sh
+++ rockPaperScissors.sh
@@ -2,4 +2,24 @@
 
 # command-line rock, paper, scissors game written in bash
 
-echo "Hello world"
+# get user choice of "rock", "paper", or "scissors"
+get_user_choice() {
+
+}
+
+# get random choice of "rock", "paper", or "scissors" from computer
+get_random_choice() {
+
+}
+
+# run 1 round of rock, paper, scissors
+play_round() {
+
+}
+
+# run rock, paper, scissors game
+play_game() {
+
+}
+
+play_game
