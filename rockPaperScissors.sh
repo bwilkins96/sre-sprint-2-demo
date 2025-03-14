25ec9375e210ca4f42c2854519838006666ab6b2
bwilkins96 <ben.jr.speed@gmail.com>
2025-03-14 19:24:58 -0400

    get random choice

--- rockPaperScissors.sh
+++ rockPaperScissors.sh
@@ -2,6 +2,22 @@
 
 # command-line rock, paper, scissors game written in bash
 
+#-----------------
+# helper functions
+#-----------------
+
+random_number() {
+    local min=$1
+    local max=$2
+
+    local res=$((RANDOM % (max - min + 1) + min))
+    echo $res
+}
+
+#---------------
+# main functions
+#---------------
+
 # get user choice of "rock", "paper", or "scissors"
 get_user_choice() {
 
@@ -9,8 +25,12 @@ get_user_choice() {
 
 # get random choice of "rock", "paper", or "scissors" from computer
 get_random_choice() {
+    local rps_array=("rock" "paper" "scissors")
+    local random_index=$( random_number 0 2 )
 
+    echo ${rps_array[$random_index]}
 }
+
 
 # run 1 round of rock, paper, scissors
 play_round() {
