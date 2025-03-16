d4b756c0102f167c0eecefa40e3a9d4d5f425be2
bwilkins96 <ben.jr.speed@gmail.com>
2025-03-16 18:46:02 -0400

    main script and moved files

---
+++ main.sh
@@ -0,0 +1,11 @@
+# starts and stops docker container with rock, paper, scissors game written in bash
+
+# must be run inside 'sre-sprint-2-demo' folder
+
+# example commands:
+# cd ./sre-sprint-2-demo
+# ./main.sh
+
+./scripts/start.sh
+
+./scripts/cleanUp.sh
