8ab2e0375afbcfe83fbcc1efebad9545023b0b77
bwilkins96 <ben.jr.speed@gmail.com>
2025-03-16 19:21:03 -0400

    'play alias for docker image'

--- Dockerfile
+++ Dockerfile
@@ -3,3 +3,5 @@ FROM ubuntu:22.04
 WORKDIR /home
 
 COPY ./rockPaperScissors.sh ./rockPaperScissors.sh 
+
+RUN echo "alias play=/home/rockPaperScissors.sh" >> ~/.bash_aliases
--- scripts/start.sh
+++ scripts/start.sh
@@ -2,10 +2,12 @@
 
 # builds ubuntu docker image with rock, paper, scissors game and runs container
 
-echo -e "BUILDING IMAGE: rps-image\n"
+echo -e "\nBUILDING IMAGE: rps-image\n"
 
 docker build -t rps-image .
 
-echo -e "\nRUNNING CONTAINER: rps-container\nRun './rockPaperScissors.sh' to play\nType 'exit' to quit\n"
+echo -e "\nRUNNING CONTAINER: rps-container"
+echo "Run 'play' to start playing"
+echo -e "Run 'exit' to quit\n"
 
 docker run -it --name rps-container rps-image /bin/bash
